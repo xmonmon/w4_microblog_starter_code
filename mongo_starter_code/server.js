c99ee72054b07e3a182536e7696e2936fb55994c
Brianna Veenstra <b.g.veenstra@gmail.com>
2015-07-13 18:58:31 -0700

    mongo solution tested

--- mongo_starter_code/server.js
+++ mongo_starter_code/server.js
@@ -4,47 +4,23 @@
 var express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
-    _ = require("underscore"),
-    Post = require("./models/post.js");
-
-// serve js and css files from public folder
-app.use(express.static(__dirname + '/public'));
+    _ = require("underscore");
 
 // configure bodyParser (for handling data)
 app.use(bodyParser.urlencoded({extended: true}));
 
 
-// Posts
+// serve js and css files from public folder
+app.use(express.static(__dirname + '/public'));
 
-function seedDB(){
-  // pre-seeded post data - old
-  var posts =[
-    {id: 1, author: "Alan", text: "Hiked 8 miles this weekend! Finally made it out to the waterfall."},
-    {id: 3, author: "Celeste", text: "On the other side of the cloud, a silver lining."},
-    {id: 2, author: "Bette", text: "Garden starting to produce veggies! Best tomato ever."},
-    {id: 4, author: "Daniel", text: "Been relearning geometry to help niece -- owning triangles so hard right now."},
-    {id: 5, author: "Evelyn", text: "We need team jackets!"},
-  ];
+// include mongoose
+var mongoose = require('mongoose');
 
+// include our module from the other file
+var Post = require("./models/post");
 
-  // loop through the old post seed data, and create db records for each post
-  // note that we DO NOT keep the id part  of the post --- Mongo will add an _id for us
-  console.log("posts: ", posts);
-  for (var i=0; i<posts.length; i++){
-    console.log(i);
-    // Post.create is the same as making a new Post instance and calling save on it
-    Post.create({author: posts[i].author, text: posts[i].text}, function(err, newPost){
-      console.log("creating post with author ", posts[i].author, " and text ", posts[i].text);
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(newPost);
-      }
-    });
-  }
-}
-seedDB();
-
+// connect to db
+mongoose.connect('mongodb://localhost/catchphrasely');
 
 // ROUTES
 
@@ -61,7 +37,7 @@ app.get('/', function (req, res) {
 // get all posts
 app.get('/api/posts', function (req, res) {
   // find all posts from the database 
-  var allPosts = Post.find({}, function(err, posts){
+  Post.find({}, function(err, allPosts){
     if (err){
       console.log("error: ", err);
       res.status(500).send(err);
@@ -70,38 +46,44 @@ app.get('/api/posts', function (req, res) {
       res.json(allPosts); 
     }
   });
+
 });
 
 // create new post
 app.post('/api/posts', function (req, res) {
   // use params (author and text) from request body
   // to create a new post
-  var newPost = Post.create({
-        author: req.body.author,
-        text: req.body.text
-      }, 
-      function(err, newPost){
-        if (err) {
-          console.log(err);
-        } else {
-          console.log(newPost);
-        }
-      });
-
-  // send newPost as JSON response
-  res.json(newPost);
+  Post.create({ 
+                author: req.body.author, 
+                text: req.body.text 
+              }, function(err, newPost){
+                  if (err) {
+                    console.log("error: ",err);
+                    res.status(500).send(err);
+                  } else {
+                    // send newPost as JSON response
+                    res.json(newPost);
+                  }
+                });
 });
 
 // get a single post 
 app.get('/api/posts/:id', function(req, res) {
 
   // take the value of the id from the url parameter
-  var targetId = parseInt(req.params.id);
+  // note that now we are NOT using parseInt
+  var targetId = req.params.id
 
   // find item in database matching the id
   Post.findOne({_id: targetId}, function(err, foundPost){
-    // send back post object
-    res.json(foundPost);
+    console.log(foundPost);
+    if(err){
+      console.log("error: ", err);
+      res.status(500).send(err);
+    } else {
+      // send back post object
+      res.json(foundPost);
+    }
   });
 
 });
@@ -110,7 +92,7 @@ app.get('/api/posts/:id', function(req, res) {
 app.put('/api/posts/:id', function(req, res) {
 
   // take the value of the id from the url parameter
-  var targetId = parseInt(req.params.id);
+  var targetId = req.params.id;
 
   // find item in `posts` array matching the id
   Post.findOne({_id: targetId}, function(err, foundPost){
@@ -145,7 +127,7 @@ app.put('/api/posts/:id', function(req, res) {
 app.delete('/api/posts/:id', function(req, res) {
 
   // take the value of the id from the url parameter
-  var targetId = parseInt(req.params.id);
+  var targetId = req.params.id;
 
  // remove item from the db that matches the id
   Post.remove({_id: targetId}, function(err, foundPosts){
